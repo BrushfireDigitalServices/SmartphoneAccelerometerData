519fd4a394d8e567d42f26bafc90417bcdb5b0c7
Henry Dwyer <HenryJDwyer@gmail.com>
2015-03-20 21:04:44 -0600

    run_analysis.R is now a function which will concatenate test and training data (in that order) for accelerometer data and subject data. More work to do, including implementing remaining requirements and refactoring to clean up name and memory space.

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,44 @@
-## You should create one R script called run_analysis.R that does the following. 
+run_analysis <- function() {
+    ## You should create one R script called run_analysis.R that does the following. 
+    
+    
+    ## 1) Merges the training and the test sets to create one data set:
+    ## Read in the subject data from test set, put in $Subject variable
+    ## from "test/subject_test.txt"
+    
+    subjTest <- read.table("../UCI HAR Dataset/test/subject_test.txt")
+    ## Read in the subject data from training set
+    ## from "train/subject_train.txt"
+    subjTrain <- read.table("../UCI HAR Dataset/train/subject_train.txt")
+    ## append to $Subject and convert to factor
+    subjMerged <- rbind(subjTest, subjTrain)
+    subjMerged$V1 <- as.factor(subjMerged$V1)
+    ## Rename V1 to Subject
+    setnames(subjMerged, old = "V1", new = "Subject")
+    ## Remove subjTest and subjTrain from namespace
+    rm(subjTest, subjTrain)
+    
 
-## Merges the training and the test sets to create one data set.
-## Extracts only the measurements on the mean and standard deviation for each measurement. 
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names. 
-
-## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+    ## Read in the activity data from test set, put in $Activity variable
+    ## Read in the activity data from training set, append to $Activity
+    
+    ## 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+    
+    ## Read from x_test to get accelerometer data, put in appropriate variables
+    testAccel <- read.table("../UCI HAR Dataset/test/X_test.txt")
+    ## Read from x_train to get accelerometer data, put in appropriate variables
+    trainAccel <- read.table("../UCI HAR Dataset/train/X_train.txt")
+    accelMerged <- rbind(testAccel, trainAccel)
+    ## read in names of variables
+    featureNames <- read.table("../UCI HAR Dataset/features.txt", colClasses = c("numeric", "character"))[[2]]
+    featureNames <- make.names(featureNames, unique = T)
+    ## Get column indices for measurements on the mean and standard deviation
+    colsToKeep <- c(grep("mean", featureNames, ignore.case = TRUE), grep("std", featureNames, ignore.case = TRUE))
+    
+    accelMerged[,colsToKeep]
+    ## 3) Uses descriptive activity names to name the activities in the data set
+    ## 4) Appropriately labels the data set with descriptive variable names. 
+    ## Create labels vector from features.txt
+    
+    ## 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+}
